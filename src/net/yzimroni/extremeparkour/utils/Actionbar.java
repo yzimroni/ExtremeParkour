79a751c618ac6caca1b159fd959a439f8837583a
yzimroni <yanay03@gmail.com>
2016-07-19 15:22:18 +0300

    Make reflect more efficient by caching the methods & fields

--- src/net/yzimroni/extremeparkour/utils/Actionbar.java
+++ src/net/yzimroni/extremeparkour/utils/Actionbar.java
@@ -1,6 +1,8 @@
 package net.yzimroni.extremeparkour.utils;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.UUID;
@@ -13,8 +15,29 @@ public class Actionbar {
 	private Plugin plugin;
 	private HashMap<UUID, String> messages;
 
+	// Actionbar nms
+	private Method createChatComponent;
+	private Constructor<?> createChatPacket;
+
+	// Player packet sending nms
+	private boolean playerNMS = false;
+	private Method playerHandle;
+	private Field playerConnection;
+	private Method sendPacket;
+
 	public Actionbar(Plugin p) {
 		plugin = p;
+
+		try {
+			createChatComponent = getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a",
+					String.class);
+			createChatPacket = getNMSClass("PacketPlayOutChat").getConstructor(getNMSClass("IChatBaseComponent"),
+					byte.class);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		messages = new HashMap<UUID, String>();
 		Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
 
@@ -24,6 +47,16 @@ public class Actionbar {
 
 			}
 		}, 20, 20);
+	}
+
+	public void sendActionBarRaw(Player p, String s) {
+		try {
+			Object text = createChatComponent.invoke(null, "{\"text\": \"" + s + "\"}");
+			Object actionbar = createChatPacket.newInstance(text, (byte) 2);
+			sendPacket(p, actionbar);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	private void sendBars() {
@@ -68,19 +101,6 @@ public class Actionbar {
 		sendActionBarRaw(p, s);
 	}
 
-	public void sendActionBarRaw(Player p, String s) {
-		try {
-			Object text = getNMSClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", String.class)
-					.invoke(null, "{\"text\": \"" + s + "\"}");
-			Constructor<?> actionbarConstructor = getNMSClass("PacketPlayOutChat")
-					.getConstructor(getNMSClass("IChatBaseComponent"), byte.class);
-			Object actionbar = actionbarConstructor.newInstance(text, (byte) 2);
-			sendPacket(p, actionbar);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 	private Class<?> getNMSClass(String classname) {
 		String version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3] + ".";
 		String name = "net.minecraft.server." + version + classname;
@@ -94,12 +114,28 @@ public class Actionbar {
 	}
 
 	private void sendPacket(Player player, Object packet) {
+		initPlayerNMS(player);
 		try {
-			Object handle = player.getClass().getMethod("getHandle").invoke(player);
-			Object playerConnection = handle.getClass().getField("playerConnection").get(handle);
-			playerConnection.getClass().getMethod("sendPacket", getNMSClass("Packet")).invoke(playerConnection, packet);
+			Object handle = playerHandle.invoke(player);
+			Object playerConnection = this.playerConnection.get(handle);
+			sendPacket.invoke(playerConnection, packet);
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	private synchronized void initPlayerNMS(Player player) {
+		if (!playerNMS) {
+			try {
+				playerHandle = player.getClass().getMethod("getHandle");
+				Object handle = playerHandle.invoke(player);
+				playerConnection = handle.getClass().getField("playerConnection");
+				Object playerConnection = this.playerConnection.get(handle);
+				sendPacket = playerConnection.getClass().getMethod("sendPacket", getNMSClass("Packet"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			playerNMS = true;
 		}
 	}
 
